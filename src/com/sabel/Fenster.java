f186c8c78bcb63a0fddbdb05f72e4bc5cf523e0b
Michael Kopp <michi_kopp1@web.de>
2017-10-13 08:08:46 +0200

    Ready

--- src/com/sabel/Fenster.java
+++ src/com/sabel/Fenster.java
@@ -14,10 +14,12 @@ public class Fenster extends JFrame{
     private Icon[] icon;
     private JScrollPane jScrollPane;
     private JLabel jLabel;
+    private int index;
 
 
     public Fenster() {
         super("Bilder");
+        index = 0;
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.initialComponents();
         this.initEvents();
@@ -25,15 +27,21 @@ public class Fenster extends JFrame{
         this.setVisible(true);
     }
 
+    public void setIndex(int index) {
+        this.index = index;
+    }
 
+    public int getIndex() {
+        return index;
+    }
 
     private void initialComponents() {
         //Buttons Sout
         jpSouth = new JPanel();
-        jbNextPicture = new JButton("Vorheriges Bild");
-        jbPreviousPicture = new JButton("Nächstes Bild");
-        jpSouth.add(jbNextPicture);
+        jbNextPicture = new JButton("Nächstes Bild");
+        jbPreviousPicture = new JButton("Vorheriges Bild");
         jpSouth.add(jbPreviousPicture);
+        jpSouth.add(jbNextPicture);
 
 
         //RadioButtons West
@@ -54,7 +62,8 @@ public class Fenster extends JFrame{
         jpCenter = new JPanel();
         icon = new ImageIcon[4];
         for (int i = 0; i < 4; i++) {
-            icon[i] = new ImageIcon("D:\\Kopp\\_Privat\\Programmieren\\IdeaProjects\\20171012_Bildbetrachter\\src\\com\\sabel\\bilder\\Bild" + (i+1) +".jpg");
+            //icon[i] = new ImageIcon("D:\\Kopp\\_Privat\\Programmieren\\IdeaProjects\\20171012_Bildbetrachter\\src\\com\\sabel\\bilder\\Bild" + (i+1) +".jpg");
+            icon[i] = new ImageIcon("C:\\Users\\micha\\IdeaProjects\\20171012_Bildbetrachter\\src\\com\\sabel\\bilder\\Bild" + (i+1) +".jpg");
 
 
         }
@@ -70,13 +79,65 @@ public class Fenster extends JFrame{
     }
 
     private void initEvents() {
-        jradios[1].addMouseListener(new MouseAdapter() {
+        for (int i = 0; i < 4; i++) {
+            int finalI = i;
+            jradios[i].addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    jLabel.setIcon(icon[finalI]);
+                    setIndex(finalI);
+                }
+            });
+        }
+
+        jbNextPicture.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                jLabel.setIcon(icon[1]);
+                //System.out.println(getIndex());
+                int temp = getIndex();
+                if (temp < 3){
+                    temp += 1;
+                    jLabel.setIcon(icon[temp]);
+                    setIndex(temp);
+                    jradios[temp].setSelected(true);
+                } else {
+                    jLabel.setIcon(icon[0]);
+                    setIndex(0);
+                    jradios[0].setSelected(true);
+                }
             }
         });
 
 
+        jbPreviousPicture.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int temp = getIndex();
+                if (temp == 0){
+                    jLabel.setIcon(icon[3]);
+                    setIndex(3);
+                    jradios[3].setSelected(true);
+                } else {
+                    temp -=1;
+                    jLabel.setIcon(icon[temp]);
+                    setIndex(temp);
+                    jradios[temp].setSelected(true);
+                }
+            }
+        });
+
+
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        this.addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                if (JOptionPane.showConfirmDialog(null,
+                        "Are you sure to close this window?", "Really Closing?",
+                        JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION){
+                    System.exit(0);
+                }
+            }
+        });
     }
 }
